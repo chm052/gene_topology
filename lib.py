21ff0df21265d1ef47ad6bd3df93107a49e61912
chm <mane360@yahoo.com>
2018-06-27 19:15:27 +1200

    New functions from eli - to clean up later

--- lib.py
+++ lib.py
@@ -3,7 +3,22 @@ INDEX_G2 = 3
 INDEX_SCORE = 5
 INDEX_EXTRA_DATA = 6
 
+import os 
+from multiprocessing import Pool
+import multiprocessing
+import time
+import pickle
+import itertools
+from numpy import linalg as LA
+import networkx as nx
+import pandas as pd
+import seaborn as sns
 
+
+def output_file_name(input_file):
+     return input_file.split(".")[0].split(os.sep)[-1]
+     
+    
 def read(filename, skip_header=True):
     f = open(filename)
 
@@ -39,9 +54,10 @@ def find_networks(f):
         g1 = list_line[INDEX_G1].lower()
         g2 = list_line[INDEX_G2].lower()
         score = list_line[INDEX_SCORE]
+        p = list_line[INDEX_EXTRA_DATA]
 
         # if the nodes are connected
-        if relevant_score(score):
+        if relevant_score(score) and significant_p(p):
 
             g1_index = -1
             g2_index = -1
@@ -127,14 +143,23 @@ def filter_by_seed(networks, seed):
     return seeded_networks
 
 
-def get_data_for_seeded_networks(seeded_networks, f, test):
+def get_data_for_seeded_networks(seeded_networks, f, test, output_name):
     f.seek(0)
     f.readline()  # skip header
     results = []
 
     # open the output files for writing
-    out_files = [open("output/gene_topology_output_network_" + str(index), "w")
-                 for index in range(len(seeded_networks))]
+    """
+    with open("output/gene_topology_output_network_{}".format("temp"), "w") as out_files :
+        out_files.write("Query allele name" + "\t" + "Array allele name" + "\t" +
+                        "Genetic interaction score (Îµ)" + "\t" +	
+                        "P-value	Query single mutant fitness (SMF)" + "\t" +
+                        "Array SMF" + "\t" +	"Double mutant fitness" + "\t" +
+                        "Double mutant fitness" + "\t" + "standard deviation" + "\n" )
+        """
+    out_files = [open("output/gene_topology_output_network_{}_".format(output_name)
+    + str(index), "w")
+             for index in range(len(seeded_networks))]
 
     for line in f:
         list_line = line.split("\t")
@@ -160,13 +185,13 @@ def get_data_for_seeded_networks(seeded_networks, f, test):
 
 
 # todo sort out this vs above
-def get_data_for_seeded_networks_pairs(seeded_networks, f, test):
+def get_data_for_seeded_networks_pairs(seeded_networks, f, test, output_name):
     f.seek(0)
     f.readline()  # skip header
     results = []
 
     # open the output files for writing
-    out_file = open("output/gene_topology_output_network", "w")
+    out_file = open("output/gene_topology_output_network_{}".format(output_name), "w")
 
     for line in f:
         list_line = line.split("\t")
@@ -188,3 +213,119 @@ def get_data_for_seeded_networks_pairs(seeded_networks, f, test):
     out_file.close()
 
     return results
+
+def make_network(network_file):
+    network_read = pd.read_csv(network_file , sep = "\t")
+    networkx_read = {'Query_allele_name':network_read.iloc[:,0],
+                    'Array_allele_name':network_read.iloc[:,1],
+                    'Genetic_interaction_score':network_read.iloc[:,3]}
+    networkx_costanzo_df = pd.DataFrame(networkx_read)
+    del networkx_read
+    del network_read
+    G = nx.from_pandas_edgelist(networkx_costanzo_df, 'Query_allele_name',
+                            'Array_allele_name', ['Genetic_interaction_score'])
+    return G
+
+def chunks(l, n):
+    """Divide a list of nodes `l` in `n` chunks"""
+    l_c = iter(l)
+    while 1:
+        x = tuple(itertools.islice(l_c, n))
+        if not x:
+            return
+        yield x
+        
+def _betmap(G_normalized_weight_sources_tuple):
+    """Pool for multiprocess only accepts functions with one argument.
+    This function uses a tuple as its only argument. We use a named tuple for
+    python 3 compatibility, and then unpack it when we send it to
+    `betweenness_centrality_source`
+    """
+    return nx.betweenness_centrality(*G_normalized_weight_sources_tuple)
+
+def betweenness_centrality_parallel(G, processes=None):
+    """Parallel betweenness centrality  function"""
+    p = Pool(processes=processes)
+    node_divisor = len(p._pool) * multiprocessing.cpu_count()
+    if G.order() / node_divisor < 1:
+         node_divisor = 2
+         node_chunks = list(chunks(G.nodes(), int(G.order() / node_divisor)))
+    else:   
+        node_chunks = list(chunks(G.nodes(), int(G.order() / node_divisor)))
+    num_chunks = len(node_chunks)
+    bt_sc = p.map(_betmap,
+                  zip([G] * num_chunks,
+                      [None] * num_chunks, #k
+                      [True] * num_chunks, #normalized
+                      ['weight'] * num_chunks, #weight
+                      [False] * num_chunks, #endpoints
+                      [None] * num_chunks #seed
+                      ))
+    # Reduce the partial solutions
+    bt_c = bt_sc[0]
+    for bt in bt_sc[1:]:
+        for n in bt:
+            bt_c[n] += bt[n]
+    return bt_c
+
+
+def _betmap_closeness_centrality(G_normalized_weight_sources_tuple):
+    return nx.closeness_centrality(*G_normalized_weight_sources_tuple)
+
+def closeness_centrality_parallel(G, processes=None):
+    """Parallel betweenness centrality  function"""
+    p = Pool(processes=processes)
+    node_divisor = len(p._pool) * multiprocessing.cpu_count()
+    if G.order() / node_divisor < 1:
+         node_divisor = 2
+         node_chunks = list(chunks(G.nodes(), int(G.order() / node_divisor)))
+    else:   
+        node_chunks = list(chunks(G.nodes(), int(G.order() / node_divisor)))
+    num_chunks = len(node_chunks)
+    bt_sc = p.map(_betmap_closeness_centrality,
+                  zip([G] * num_chunks,
+                      ))
+    bt_c = bt_sc[0]
+    for bt in bt_sc[1:]:
+        for n in bt:
+            bt_c[n] += bt[n]
+    return bt_c
+    
+def eigenvector_centrality(G):
+    adjacancy_matrix = nx.to_numpy_matrix(G)
+    enigvaleus = LA.eigvals(adjacancy_matrix)
+    return enigvaleus  
+
+def computing_topology(G, file_name):
+    print("Computing betweenness_centrality_parallel for:")
+    print(nx.info(G))
+    print("\tParallel version")
+    start = time.time()
+    bt = betweenness_centrality_parallel(G)
+    print("\t\tTime: %.4F" % (time.time() - start))
+    bt_list=list(bt.values())
+    with open("output/{}_betwenness_centrality.list".format(file_name), "wb") as fp: 
+        pickle.dump(bt_list, fp)
+    
+    print("Computing betweenness_centrality_parallel for:")
+    print(nx.info(G))
+    print("\tParallel version")
+    start = time.time()
+    bt = betweenness_centrality_parallel(G)
+    print("\t\tTime: %.4F" % (time.time() - start))
+    bt_list=list(bt.values())
+    with open("output/{}_betwenness_centrality.list".format(file_name), "wb") as fp: 
+        pickle.dump(bt_list, fp)
+
+    print("Computing closeness_centrality_parallel for:")
+    print("\tParallel version")
+    start = time.time()
+    bt = closeness_centrality_parallel(G)
+    print("\t\tTime: %.4F" % (time.time() - start))
+    bt_list=list(bt.values())
+    with open("output/{}_closeness_centrality.list".format(file_name), "wb") as fp: 
+        pickle.dump(bt_list, fp)
+
+    eigenvector= eigenvector_centrality(G)
+    with open("output/{}_eigenvector_centrality.list".format(file_name), "wb") as fp: 
+        pickle.dump(eigenvector, fp)  
--- main.py
+++ main.py
@@ -1,12 +1,11 @@
 from lib import *
 from sys import argv
 
-
 def main(seed_filename, data_filename, distance=-1, test=False):
     print("Open the file")
     seed_file = read(seed_filename, False)
     data_file = read(data_filename)
-
+    output_name= output_file_name(seed_filename)
     print("Make list of seed")
     seed = get_seed_list(seed_file)
     
@@ -21,17 +20,23 @@ def main(seed_filename, data_filename, distance=-1, test=False):
         seeded_networks = filter_by_seed(networks, seed)
         
         print("output")
-        results = get_data_for_seeded_networks(seeded_networks, data_file, test)
+        results = get_data_for_seeded_networks(seeded_networks,
+                                               data_file, test,output_name)
         
     else:
         print("make list of networks to distance %s" % distance)
         seeded_networks = find_cropped_networks(data_file, seed, distance)
 
         print("output")
-        results = get_data_for_seeded_networks_pairs(seeded_networks, data_file, test)
+        results = get_data_for_seeded_networks_pairs(seeded_networks,
+                                                     data_file, test,output_name)
+    print("Constructing the Network")
+    G= make_network("output/gene_topology_output_network_{}".format(output_name))
+    
+    print("Calculating topology")
+    computing_topology(G, output_name)
     
     print("Done")
-    return results
 
 
 if __name__ == "__main__":
